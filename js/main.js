9c65a398ed494c4432c60071a1b1de7ca2ba8741
serega240805 <108699300+serega240805@users.noreply.github.com>
2022-08-25 22:51:54 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-new Swiper('.companies__slider');
+new Swiper('.companies__slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 4,
+    spaceBetween: 25,
+    slidesPerGroup: 3,
+});
