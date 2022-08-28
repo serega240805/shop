d3f1138e5ed52d471fced1c4d1289d099835c429
serega240805 <108699300+serega240805@users.noreply.github.com>
2022-08-28 09:12:08 +0300

    new

--- js/main.js
+++ js/main.js
@@ -4,10 +4,29 @@ new Swiper('.companies__slider', {
         clickable: true,
     },
     slidesPerView: 4,
-    spaceBetween: 25,
-    slidesPerGroup: 3,
+    spaceBetween: 105,
+    slidesPerGroup: 1,
+    freeMode: true,
+    loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
+    },
+    mousewheel: {
+        sensivity: 1,
+    },
+    breakpoints: {
+        1160: {
+            slidesPerView: 4,
+            spaceBetween: 25,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 105,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 105,
+        },
     },
 });
